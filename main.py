32300f48203e074175be90aca3b31bb50a756b59
Wojciech Czarnowski <czarnowskiwojtek@gmail.com>
2025-01-08 12:46:24 +0100

    preparing txt files

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print('Hello')
+import matplotlib.pylab as plt
+import numpy as np
+import pandas
+import glob
+import librosa
+
+
+#input_file = "AudioData/validation_list.txt"
+input_file = "AudioData/testing_list.txt"
+
+#output_file = "AudioData/validation_list_filtered.txt"
+output_file = "AudioData/testing_list_filtered.txt"
+
+
+exclusions = ["cat", "wow", "visual","tree","sheila","marvin","learn","house","happy","forward","follow","dog","cat","bird","bed","backward","background_noise"]
+
+
+with open(input_file, "r") as infile:
+    paths = infile.readlines()
+
+
+filtered_paths = [
+    path for path in paths if not any(path.startswith(exclusion + "/") for exclusion in exclusions)
+]
+
+with open(output_file, "w") as outfile:
+    outfile.writelines(filtered_paths)
+
+print(f"Sciezke zapisano w '{output_file}'.")
