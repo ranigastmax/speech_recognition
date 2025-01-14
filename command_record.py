2cb260ab830fd7d51271b2e828c814fbaaf581ff
Wojciech Czarnowski <czarnowskiwojtek@gmail.com>
2025-01-14 18:51:11 +0100

    22 nagrania Wojtek

--- command_record.py
+++ command_record.py
@@ -1,11 +1,22 @@
 import sounddevice as sd
 from scipy.io.wavfile import write
+import os
+
+def record_audio(OUTPUT_FILE, duration, sample_rate):
 
 
+    folder_name = "Our_Records_5s"
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
 
-def record_audio(file_name, duration, sample_rate):
+    full_path = os.path.join(folder_name, OUTPUT_FILE)
+
+    if not full_path.endswith(".wav"):
+        full_path += ".wav"
+
     print(f"Nagrywanie... Powiedz coś! (czas nagrywania: {duration} sekund)")
     audio = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=1, dtype='int16')
     sd.wait()
-    write(file_name, sample_rate, audio)
-    print(f"Nagranie zapisane jako {file_name}")
+
+    write(full_path, sample_rate, audio)
+    print(f"Nagranie zapisane jako {full_path}")
